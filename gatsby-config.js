4200cec3ad20cdcbfc61077bdf591533083a51e3
Alvin Bryan <hello@alvin.codes>
2024-01-17 16:14:10 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Directus Gatsby Gallery`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
